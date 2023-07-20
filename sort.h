a93b4644fb106ae0d8cddaeda8ab4d03bf9c1fa1
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-07-20 19:20:16 +0100

    Update Files, sort.h

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
 #ifndef SORT_H
-#define SORT_H 
+#define SORT_H
 
 /**
  * struct listint_s - Doubly linked list node
@@ -11,9 +11,13 @@
 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+/* PROTOTYPES */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 #endif /* SORT_H */
